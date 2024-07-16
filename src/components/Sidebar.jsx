0d68291b492717ab0cf90c573667f6012d8eb50a
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-16 05:54:14 +0300

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "../App.css";
 
+import { SidebarData } from "./SidebarData";
+
 const Sidebar = () => {
-  return <div className="Sidebar">Sidebar</div>;
+  return (
+    <div className="Sidebar">
+      <ul>
+        {SidebarData.map((item, idx) => (
+          <li
+            className=""
+            key={idx}
+            onClick={() => {
+              window.location.href(item.link);
+            }}
+          >
+            <div className="">{item.icon}</div>
+            <div className="">{item.title}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
