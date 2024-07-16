356206285f49b4e48a56ba6e7b597c5b16c0054e
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-16 05:54:27 +0300

    Update SidebarData.js

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -1,7 +1,40 @@
 import React from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import EmailIcon from "@mui/icons-material/Email";
+import InsertChartIcon from "@mui/icons-material/InsertChart";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import Diversity3Icon from "@mui/icons-material/Diversity3";
+import PermMediaIcon from "@mui/icons-material/PermMedia";
 
-const SidebarData = () => {
-  return <div></div>;
-};
-
-export default SidebarData;
+export const SidebarData = [
+  {
+    title: "Home",
+    icon: <HomeIcon />,
+    link: "/home",
+  },
+  {
+    title: "Mailbox",
+    icon: <EmailIcon />,
+    link: "/mailbox",
+  },
+  {
+    title: "Analytics",
+    icon: <InsertChartIcon />,
+    link: "/analytics",
+  },
+  {
+    title: "Dashboard",
+    icon: <DashboardIcon />,
+    link: "/dashboard",
+  },
+  {
+    title: "Friends",
+    icon: <Diversity3Icon />,
+    link: "/friends",
+  },
+  {
+    title: "Images",
+    icon: <PermMediaIcon />,
+    link: "/images",
+  },
+];
